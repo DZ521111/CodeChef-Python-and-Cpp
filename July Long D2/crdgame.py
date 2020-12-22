bb740d12c334634bcfd2aecfeaf6632de3aa9dba
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-23 00:17:48 +0530

    crdgame code added

---
+++ July Long D2/crdgame.py
@@ -0,0 +1,25 @@
+'''
+Author : Dhruv B Kakadiya
+'''
+# cook your dish here
+for _ in range(int(input())):
+    rounds = int(input())
+    chef_p, morty_p = 0, 0
+    for i in range(rounds):
+        chef, morty = input().split()
+        a = sum(list(map(int, chef)))
+        b = sum(list(map(int, morty)))
+        if (a == b):
+            chef_p += 1
+            morty_p += 1
+        elif (a > b):
+            chef_p += 1
+        else:
+            morty_p += 1
+    else:
+        if (chef_p > morty_p):
+            print(0, chef_p)
+        elif (chef_p < morty_p):
+            print(1, morty_p)
+        else:
+            print(2, chef_p)
