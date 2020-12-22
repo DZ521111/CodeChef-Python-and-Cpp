b85009310337090c7af179260442bbb0b957d79e
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-23 00:16:30 +0530

    first code added

---
+++ July Long D2/chefstr1.py
@@ -0,0 +1,13 @@
+'''
+Author : Dhruv B Kakadiya
+'''
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    strings = list(map(int, input().split()))
+    sum_ = 0
+    i = 1
+    while (i < n):
+        sum_ += (abs(strings[i] - strings[i-1]) - 1)
+        i += 1
+    print(sum_)
