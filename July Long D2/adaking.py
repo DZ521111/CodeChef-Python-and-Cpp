6101d0c81609b8e38ab41d79c1ce8583633d70a3
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-23 00:18:40 +0530

    crdgame code added

---
+++ July Long D2/adaking.py
@@ -0,0 +1,505 @@
+'''
+Author : Dhruv B Kakadiya
+'''
+for _ in range(int(input())):
+    k = int(input())
+    if (k == 1):
+        print("OX......")
+        print("XX......")
+        for i in range(6):
+            print("........")
+        #print()
+    elif (k == 2):
+        print("O.X.....")
+        print("XXX.....")
+        for i in range(6):
+            print("........")
+        #print()
+    elif (k == 3):
+        print("O.X.....")
+        print(".XX.....")
+        print("XX......")
+        for i in range(5):
+            print("........")
+        #print()
+    elif (k == 4):
+        print("O.X.....")
+        print("..X.....")
+        print("XXX.....")
+        for i in range(5):
+            print("........")
+        #print()
+    elif (k == 5):
+        print("O..X....")
+        print("..XX....")
+        print("XXX.....")
+        for i in range(5):
+            print("........")
+        #print()
+    elif (k == 6):
+        print(".O.X....")
+        print("...X....")
+        print("XXXX....")
+        for i in range(5):
+            print("........")
+        #print()
+    elif (k == 7):
+        print("...X....")
+        print(".O.X....")
+        print(".XXX....")
+        print("XX......")
+        for i in range(4):
+            print("........")
+        #print()
+    elif (k == 8):
+        print("...X....")
+        print(".O.X....")
+        print("..XX....")
+        print("XXX.....")
+        for i in range(4):
+            print("........")
+        #print()
+    elif (k == 9):
+        print("...X....")
+        print(".O.X....")
+        print("...X....")
+        print("XXXX....")
+        for i in range(4):
+            print("........")
+    elif (k == 10):
+        print("....X...")
+        print(".O.XX...")
+        print("...X....")
+        print("XXXX....")
+        for i in range(4):
+            print("........")
+    elif (k == 11):
+        print("....X...")
+        print(".O..X...")
+        print("...XX...")
+        print("XXXX....")
+        for i in range(4):
+            print("........")
+    elif (k == 12):
+        print("....X...")
+        print(".O..X...")
+        print("....X...")
+        print("XXXXX...")
+        for i in range(4):
+            print("........")
+    elif (k == 13):
+        print(".....X..")
+        print("..O.XX..")
+        print("....X...")
+        print("XXXXX...")
+        for i in range(4):
+            print("........")
+    elif (k == 14):
+        print(".....X..")
+        print("..O..X..")
+        print("....XX..")
+        print("XXXXX...")
+        for i in range(4):
+            print("........")
+    elif (k == 15):
+        print(".....X..")
+        print("..O..X..")
+        print(".....X..")
+        print("XXXXXX..")
+        for i in range(4):
+            print("........")
+    elif (k == 16):
+        print("......X.")
+        print("..O..XX.")
+        print(".....X..")
+        print("XXXXXX..")
+        for i in range(4):
+            print("........")
+    elif (k == 17):
+        print("......X.")
+        print("..O...X.")
+        print(".....XX.")
+        print("XXXXXX..")
+        for i in range(4):
+            print("........")
+    elif (k == 18):
+        print("......X.")
+        print("..O...X.")
+        print("......X.")
+        print("XXXXXXX.")
+        for i in range(4):
+            print("........")
+    elif (k == 19):
+        print(".......X")
+        print("...O..XX")
+        print("......X.")
+        print("XXXXXXX.")
+        for i in range(4):
+            print("........")
+    elif (k == 20):
+        print(".......X")
+        print("...O...X")
+        print("......XX")
+        print("XXXXXXX.")
+        for i in range(4):
+            print("........")
+    elif (k == 21):
+        print(".......X")
+        print("...O...X")
+        print(".......X")
+        print("XXXXXXXX")
+        for i in range(4):
+            print("........")
+    elif (k == 22):
+        print("........")
+        print("...O...X")
+        print(".......X")
+        print("XXXXXXXX")
+        for i in range(4):
+            print("........")
+    elif (k == 23):
+        print("........")
+        print("...O....")
+        print(".......X")
+        print("XXXXXXXX")
+        for i in range(4):
+            print("........")
+    elif (k == 24):
+        print("........")
+        print("...O....")
+        print("........")
+        print("XXXXXXXX")
+        for i in range(4):
+            print("........")
+    elif (k == 25):
+        print("........")
+        print("...O....")
+        print("........")
+        print(".XXXXXXX")
+        print("XX......")
+        for i in range(3):
+            print("........")
+    elif (k == 26):
+        print("........")
+        print("...O....")
+        print("........")
+        print("..XXXXXX")
+        print("XXX.....")
+        for i in range(3):
+            print("........")
+    elif (k == 27):
+        print("........")
+        print("...O....")
+        print("........")
+        print("...XXXXX")
+        print("XXXX....")
+        for i in range(3):
+            print("........")
+    elif (k == 28):
+        print("........")
+        print("...O....")
+        print("........")
+        print("....XXXX")
+        print("XXXXX...")
+        for i in range(3):
+            print("........")
+    elif (k == 29):
+        print("........")
+        print("...O....")
+        print("........")
+        print(".....XXX")
+        print("XXXXXX..")
+        for i in range(3):
+            print("........")
+    elif (k == 30):
+        print("........")
+        print("...O....")
+        print("........")
+        print("......XX")
+        print("XXXXXXX.")
+        for i in range(3):
+            print("........")
+    elif (k == 31):
+        print("........")
+        print("...O....")
+        print("........")
+        print(".......X")
+        print("XXXXXXXX")
+        for i in range(3):
+            print("........")
+    elif (k == 32):
+        print("........")
+        print("...O....")
+        print("........")
+        print("........")
+        print("XXXXXXXX")
+        for i in range(3):
+            print("........")
+    elif (k == 33):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print(".XXXXXXX")
+        print("XX......")
+        for i in range(2):
+            print("........")
+    elif (k == 34):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("..XXXXXX")
+        print("XXX.....")
+        for i in range(2):
+            print("........")
+    elif (k == 35):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("...XXXXX")
+        print("XXXX....")
+        for i in range(2):
+            print("........")
+    elif (k == 36):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("....XXXX")
+        print("XXXXX...")
+        for i in range(2):
+            print("........")
+    elif (k == 37):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print(".....XXX")
+        print("XXXXXX..")
+        for i in range(2):
+            print("........")
+    elif (k == 38):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("......XX")
+        print("XXXXXXX.")
+        for i in range(2):
+            print("........")
+    elif (k == 39):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print(".......X")
+        print("XXXXXXXX")
+        for i in range(2):
+            print("........")
+    elif (k == 40):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("XXXXXXXX")
+        for i in range(2):
+            print("........")
+    elif (k == 41):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print(".XXXXXXX")
+        print("XX......")
+        print("........")
+    elif (k == 42):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("..XXXXXX")
+        print("XXX.....")
+        print("........")
+    elif (k == 43):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("...XXXXX")
+        print("XXXX....")
+        print("........")
+    elif (k == 44):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("....XXXX")
+        print("XXXXX...")
+        print("........")
+    elif (k == 45):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print(".....XXX")
+        print("XXXXXX..")
+        print("........")
+    elif (k == 46):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("......XX")
+        print("XXXXXXX.")
+        print("........")
+    elif (k == 47):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print(".......X")
+        print("XXXXXXXX")
+        print("........")
+    elif (k == 48):
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("........")
+        print("XXXXXXXX")
+        print("........")
+    elif (k == 49):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print(".XXXXXXX")
+        print("XX......")
+    elif (k == 50):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("..XXXXXX")
+        print("XXX.....")
+    elif (k == 51):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("...XXXXX")
+        print("XXXX....")
+    elif (k == 52):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("....XXXX")
+        print("XXXXX...")
+    elif (k == 53):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print(".....XXX")
+        print("XXXXXX..")
+    elif (k == 54):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("......XX")
+        print("XXXXXXX.")
+    elif (k == 55):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print(".......X")
+        print("XXXXXXXX")
+    elif (k == 56):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("........")
+        print("XXXXXXXX")
+    elif (k == 57):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("........")
+        print(".XXXXXXX")
+    elif (k == 58):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("........")
+        print("..XXXXXX")
+    elif (k == 59):
+        print("........")
+        print("........")
+        print("........")
+        print("....O...")
+        print("........")
+        print("........")
+        print("........")
+        print("...XXXXX")
+    elif (k == 60):
+        print("...X....")
+        print(".O.X....")
+        print("..XX....")
+        for i in range(5):
+            print("........")
+    elif (k == 61):
+        print("...OX...")
+        print("...X.X..")
+        for i in range(6):
+            print("........")
+    elif (k == 62):
+        print("...OX...")
+        print("...X....")
+        for i in range(6):
+            print("........")
+    elif (k == 63):
+        print("..OX....")
+        for i in range(7):
+            print("........")
+    elif (k == 64):
+        print("...O....")
+        for i in range(7):
+            print("........")
+        
