425024b6af59fd6fdb5a707f688dc6f469ae77be
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-27 19:19:07 +0530

    chefinsq code added

--- practice Codes/chefinsq.py
+++ practice Codes/chefinsq.py
@@ -6,12 +6,12 @@ Author : Dhruv B Kakadiya
 # cook your dish here
 import math
 for _ in range(int(input())):
-    n,k=map(int, input().split())
-    l=list(map(int,input().split()))
+    n, k = map(int, input().split())
+    l = list(map(int,input().split()))
     l.sort()
-    e=l[k-1]
-    l1=l[k:]
-    n=l.count(e)
-    r=n-l1.count(e)
-    c=math.factorial(n)//(math.factorial(r)*math.factorial(n-r))
+    e = l[k-1]
+    l1 = l[k:]
+    n = l.count(e)
+    r = n-l1.count(e)
+    c = math.factorial(n) // (math.factorial(r)*math.factorial(n-r))
     print(c)
