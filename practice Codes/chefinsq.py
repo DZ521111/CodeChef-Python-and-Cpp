1822cc5dfe5095503294c53e438826660efe5982
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-27 19:18:40 +0530

    chefinsq code added

---
+++ practice Codes/chefinsq.py
@@ -0,0 +1,17 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+
+# cook your dish here
+import math
+for _ in range(int(input())):
+    n,k=map(int, input().split())
+    l=list(map(int,input().split()))
+    l.sort()
+    e=l[k-1]
+    l1=l[k:]
+    n=l.count(e)
+    r=n-l1.count(e)
+    c=math.factorial(n)//(math.factorial(r)*math.factorial(n-r))
+    print(c)
