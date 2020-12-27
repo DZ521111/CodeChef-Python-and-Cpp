d272059e92da6721a6de4a48e85c17ec8aeafb62
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-27 19:16:14 +0530

    cash code added

---
+++ practice Codes/cash.py
@@ -0,0 +1,11 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    coins = [int(x) for x in input().split()]
+    sumc=0
+    for i in coins:
+        sumc+=i%k
+    print(sumc%k)
