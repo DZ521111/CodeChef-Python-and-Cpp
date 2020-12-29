aaf0a8a03ec2412705713327c7431e043e5495ad
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-29 18:57:14 +0530

    recndnos updated

--- practice Codes/recndnos.py
+++ practice Codes/recndnos.py
@@ -6,9 +6,9 @@ Author : Dhruv B Kakadiya
 for _ in range(int(input())):
     n = int(input())
     A = list(map(int, input().split()))
-    for i in range(n-1):
-        if A[i] == A[i+1]:
-            A[i+1] = 'x'
+    for i in range(n - 1):
+        if A[i] == A[i + 1]:
+            A[i + 1] = 'x'
     y = A[0]
     count = A.count(A[0])
     for j in range(n):
