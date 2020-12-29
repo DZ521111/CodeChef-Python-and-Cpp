c5605a3713a918899d19806ed64181bc6c1da436
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-29 18:55:23 +0530

    ishvala code added

--- practice Codes/recndstr.py
+++ practice Codes/recndstr.py
@@ -3,20 +3,20 @@ Author : Dhruv B Kakadiya
 
 '''
 
-t = int(input())
-while t:
+tt = int(input())
+while tt:
     s = input()
     if len(s) == 1:
         print('YES')
     else:
         l = list(s)
-        res_l=[]
+        res_l = []
         for i in range(len(l)):
-            res_l.append(l[i-1])
-        res_r = l[1:]
+            res_l.append(l[i - 1])
+        res_r = l[1 : ]
         res_r.append(l[0])
         if res_l == res_r:
             print('YES')
         else:
             print('NO')
-    t = t-1
+    tt = tt - 1
