0186fe59050fd1342a1699385aae917d50ef7f44
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-29 18:58:43 +0530

    spamclas updated

--- practice Codes/spamclas.py
+++ practice Codes/spamclas.py
@@ -4,19 +4,19 @@ Author : Dhruv B Kakadiya
 '''
 
 for _ in range(int(input())):
-    n,minx,maxx=map(int,input().split())
-    odd=(maxx-minx)//2
-    if(maxx%2!=0 or minx%2!=0):
-        odd+=1
-    even=(maxx-minx+1)-odd
+    n, minx, maxx = map(int, input().split())
+    odd = (maxx - minx) // 2
+    if(maxx % 2 != 0 or minx % 2 != 0):
+        odd += 1
+    even = (maxx - minx + 1) - odd
     for i in range(n):
-        w,b=map(int,input().split())
-        if(w%2==0 and b%2==0):
-            even=even+odd
-            odd=0
-        elif(w%2==0 and b%2!=0):
-            odd=odd+even
-            even=0
-        elif(w%2!=0 and b%2!=0):
-            even,odd=odd,even
-    print(even,odd)
+        w, b = map(int, input().split())
+        if(w % 2 == 0 and b % 2 == 0):
+            even = even + odd
+            odd = 0
+        elif(w % 2 == 0 and b % 2 != 0):
+            odd = odd + even
+            even = 0
+        elif(w % 2 != 0 and b % 2 != 0):
+            even, odd = odd, even
+    print(even, odd)
