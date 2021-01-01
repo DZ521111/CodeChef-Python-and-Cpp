b0b7c3750be5340dc361fd82426c314285011c8b
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-01 23:43:55 +0530

    trainset code added

---
+++ practice Codes/wwalk.py
@@ -0,0 +1,21 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+# cook your dish here
+for _ in range(int(input())):
+    temp = int(input())
+    Alice = list(map(int,input().split()))
+    Bob = list(map(int,input().split()))
+    tot_Alice = 0
+    tot_Bob = 0
+    total = []
+    for j,k in zip(Alice,Bob):
+        if (tot_Alice == tot_Bob) and (j == k):
+            total.append(j)
+            tot_Alice += j 
+            tot_Bob += k 
+        else:
+            tot_Alice += j 
+            tot_Bob += k 
+    print(sum(total))
