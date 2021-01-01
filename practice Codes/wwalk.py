433ec119b8118802bf4973aad70485cffc4f4e0c
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-01 23:44:34 +0530

    trainset code added

--- practice Codes/wwalk.py
+++ practice Codes/wwalk.py
@@ -5,17 +5,17 @@ Author : Dhruv B Kakadiya
 # cook your dish here
 for _ in range(int(input())):
     temp = int(input())
-    Alice = list(map(int,input().split()))
-    Bob = list(map(int,input().split()))
+    Alice = list(map(int, input().split()))
+    Bob = list(map(int, input().split()))
     tot_Alice = 0
     tot_Bob = 0
     total = []
-    for j,k in zip(Alice,Bob):
+    for j, k in zip(Alice, Bob):
         if (tot_Alice == tot_Bob) and (j == k):
             total.append(j)
-            tot_Alice += j 
-            tot_Bob += k 
+            tot_Alice += j
+            tot_Bob += k
         else:
-            tot_Alice += j 
-            tot_Bob += k 
+            tot_Alice += j
+            tot_Bob += k
     print(sum(total))
