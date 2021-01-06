f75b52a2d6d03c71a83339727711050da17113bd
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-06 15:45:53 +0530

    challenge code added!

---
+++ practice Codes/challenge.py
@@ -0,0 +1,15 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+
+# cook your dish here
+import random
+import sys
+alpha=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+t,n,k=map(int,input().split())
+for _ in range(t):
+    for printinng in range(n):
+        a=''.join(random.choice(alpha) for iter in range(10))
+        print(a,flush=True)
+    score=int(input())
