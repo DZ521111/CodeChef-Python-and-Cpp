6563c9e554dcdafb87cb0a418445adcd635af9c6
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-07 20:42:26 +0530

    luck4 code updated!

---
+++ practice Codes/powk.py
@@ -0,0 +1,33 @@
+from collections import defaultdict as dd,deque
+t=int(input())
+while t:
+    pres=dd(int)
+    d=dd(list)
+    n=int(input())
+    m=int(input())
+    ind=dd(int)
+    for i in range(m):
+        u,v=input().split()
+        if(pres[(u,v)]==0):
+            d[u].append(v)
+            pres[(u,v)]==1
+            ind[v]+=1
+    q=deque([])
+    for i in d:
+        if(ind[i]==0):
+            q.append(i)
+    res=deque()
+    while q:
+        a=q.pop()
+        res.appendleft(a)
+        for i in d[a]:
+            ind[i]-=1
+            if(ind[i]==0):
+                q.appendleft(i)
+    if(len(res)==n):
+        print("YES")
+        for i in res:
+            print(i)
+    else:
+        print("NO")
+    t-=1
