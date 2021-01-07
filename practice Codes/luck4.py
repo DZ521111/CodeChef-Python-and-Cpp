599829320e90b34e7eba390de2d2ea6727725df6
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-07 20:41:20 +0530

    luck4 code updated!

---
+++ practice Codes/luck4.py
@@ -0,0 +1,8 @@
+result = []
+for _ in range(int(input())):
+    num = int(input())
+    num_string = str(num)
+    count_ = num_string.count('4')
+    result.append(count_)
+for i in result:
+    print(i)
