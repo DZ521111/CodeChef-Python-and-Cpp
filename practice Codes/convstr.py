d24cd2932080e64e6858ba93e79865cc08076c78
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-07 20:40:38 +0530

    code updated!

--- practice Codes/convstr.py
+++ practice Codes/convstr.py
@@ -12,45 +12,45 @@ for _ in range(int(input())):
     da = dict()
     cfg = 0
     ans = []
-    for i in range(97,97+26):
-        da[chr(i)]=set()
-        db[chr(i)]=set()
+    for i in range(97, 97 + 26):
+        da[chr(i)] = set()
+        db[chr(i)] = set()
     for i in range(N):
         da[A[i]].add(i)
         db[B[i]].add(i)
-        if A[i] < B[i] :
+        if (A[i] < B[i]) :
             print(-1)
             cfg = 1
             break
     count = 0
     cfr = 0
-    if cfg == 0 :
-        for i in range(97+25,96,-1):
+    if (cfg == 0) :
+        for i in range(97 + 25, 96, -1):
             ch = chr(i)
-            if db[ch] :
+            if (db[ch]) :
                 if da[ch] :
                     c = 0
                     ind = db[ch].union(da[ch])
                     for j in ind:
-                        if A[j] != ch :
+                        if (A[j] != ch) :
                             c += 1
                             val = A[j]
                             da[val].discard(j)
                             A[j] = ch
-                    if c > 0 :
+                    if (c > 0) :
                         count += 1
                         ans.append(list(ind))
                 else:
                     print(-1)
                     cfr = 1
                     break
-        if cfr == 0 :
+        if (cfr == 0) :
             print(count)
             for x in ans :
                 le = len(x)
-                print(le,end=" ")
+                print(le, end=" ")
                 for i in range(le):
-                    print(x[i],end=" ")
+                    print(x[i], end=" ")
                 print()
 
 
