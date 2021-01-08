92b31ecbd3073a63b0118741d3183517986598b0
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-08 20:08:05 +0530

    numfact code updated!

--- practice Codes/numfact.py
+++ practice Codes/numfact.py
@@ -18,29 +18,25 @@ sys.stdout = _OUTPUT_BUFFER
 def write():
     sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
 def factor(n):
-    s = d.setdefault(2,0)
-    while n%2==0:
-        d[2]+=1
-        n//=2
-    for j in range(3,int(math.sqrt(n)+1),2):
-        if n%j==0:
-            s=d.setdefault(j,0)
-    
-            while n%j==0:
-                d[j]+=1
-                n//=j
-    if n>2:
-        s=d.setdefault(n,0)
-        d[n]+=1
-t=int(input())
-for i in range(t):
-    n=int(input())
-    a=list(map(int,input().split()))
-    d={}
+    while (n % 2 == 0):
+        d[2] += 1
+        n //= 2
+    for j in range(3, int(math.sqrt(n) + 1), 2):
+        if (n % j == 0):
+            while (n % j == 0):
+                d[j] += 1
+                n //= j
+    if (n > 2):
+        d[n] += 1
+
+for i in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    d = {}
     for j in a:
         factor(j)
-    p=1
+    p = 1
     for j in d:
-        p*=(d[j]+1)
+        p *= (d[j] + 1)
     print(p)
 
