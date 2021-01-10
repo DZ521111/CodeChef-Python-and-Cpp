8664113aa86f1467464364991fb4601da9469db5
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-10 10:22:34 +0530

    matmex.py code added!

---
+++ practice Codes/maxmex.py
@@ -0,0 +1,20 @@
+tc=int(input())
+for t in range(tc):
+    (n,m)=map(int,input().split())
+    arr=list(map(int,input().split()))
+    if min(arr)==m:
+        print(n-arr.count(m))
+        continue
+    temp=min(arr)
+    flg=0
+    arrkaset=set(arr)
+    for i in range(temp,m):
+        if i in arrkaset:
+            pass
+        else:
+            flg=1
+            break
+    if flg==1:
+        print(-1)
+    else:
+        print(n-arr.count(m))
