c6077effdc3222cc1374c47489d316f5cb9a8cde
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-18 21:56:35 +0530

    covid19b code updated!

---
+++ September Long D2/covid19B.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    num = list(map(int, input().split()))
+    take = []
+    for i in range(len(num)):
+        countL, countR = 0, 0
+        tempL, tempR = num[ : i + 1], num[i + 1 : ]
+        countL = sum(x > num[i] for x in tempL)
+        countR = sum(y < num[i] for y in tempR)
+        take.append(countR + countL + 1)
+    if (n == 3):
+        print(min(take), max(take))
+    elif (n == 4):
+        print(min(take) + 1, max(take))
+    else:
+        print(min(take) + 1, max(take))
