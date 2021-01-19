cafd9453b4b7ec2a5a99e03421f2c01b56e5fd1b
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-19 16:50:07 +0530

    finxor code updated!

--- September Long D2/finxor.py
+++ September Long D2/finxor.py
@@ -7,27 +7,27 @@ Created on Thu Sep 10 18:44:28 2020
 def find_xor(n):
     ans, lst = 0, []
     for i in range(1, 21):
-        print(1, 2**i, flush=True)
+        print(1, 2 ** i, flush = True)
         x = int(input())
         lst.append(x)
     lst.reverse()
-    sum_ = lst[0] - n*(2**len(lst))
+    sum_ = lst[0] - n*(2 ** len(lst))
     for i in range(1, len(lst)):
         if (lst[i] >= sum_):
-            lst[i] = ((n - (lst[i]-sum_)/(2**(len(lst)-i)))/2)
+            lst[i] = ((n - (lst[i] - sum_) / (2 ** (len(lst) - i))) / 2)
         else:
-            lst[i] = (n + (sum_-lst[i])/(2**(len(lst)-i)))/2
+            lst[i] = (n + (sum_ - lst[i])/(2 ** (len(lst) - i))) / 2
     for i in range(1, len(lst)):
-        if (lst[i]%2 != 0):
-            ans += 2**(len(lst)-i)
-    if (sum_%2!=0):
+        if (lst[i] % 2 != 0):
+            ans += 2 ** (len(lst) - i)
+    if (sum_ % 2 != 0):
         ans += 1
-    print(2, ans, flush=True)
+    print(2, ans, flush = True)
     res = int(input())
-    return res
+    return (res)
 
 for _ in range(int(input())):
     n = int(input())
     if (not find_xor(n)):
         break
-    print(flush=True)
+    print(flush = True)
